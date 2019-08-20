9db29b3c2fb6f2ab0c97c9ac5326cf32c05be47c
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2019-08-20 07:13:37 -0400

    update for idmi

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,29 +12,42 @@ import Sponsors from '../Sponsors/Sponsors';
 import Faq from '../Faq/Faq';
 import Footer from '../Footer/Footer';
 
+import { createGlobalStyle } from 'styled-components';
+
 import Banner1Image from '../Banner1/banner1.png';
+
+
+const MainAppStyling = createGlobalStyle`
+  body, html {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    background-color: ${props => props.theme.dark3};
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Hero />
-        <Speakers />
-        <Details />
-        <Map />
-        <Schedule />
-        <Banner1
-          bannerImage={Banner1Image}
-          bannerImageAlt='Lorem ipsum dolor est'
-          bannerTitle='Lorem Ipsum'
-          bannerSubtitle='Vestibulum purus quam.'
-          bannerDescription='Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere. Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere.'
-        />
-        <PurchaseOptions />
-        <Sponsors />
-        <Faq />
-        <Footer />
-      </div>
+        <div className='App'>
+          <MainAppStyling />
+          <Hero />
+          <Speakers />
+          <Details />
+          <Map />
+          <Schedule />
+          <Banner1
+            bannerImage={Banner1Image}
+            bannerImageAlt='Lorem ipsum dolor est'
+            bannerTitle='Lorem Ipsum'
+            bannerSubtitle='Vestibulum purus quam.'
+            bannerDescription='Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere. Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere.'
+          />
+          <PurchaseOptions />
+          <Sponsors />
+          <Faq />
+          <Footer />
+        </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,52 @@ import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+
+const theme = {
+  /**
+   * Use these colors
+   */
+  accent1: "#b81a68",
+  accent2: "#63d9c9",
+  accent2Light: "#55decc",
+  accent2Dark: "rgb(16,48,51)",
+  dark: "#575756",
+  dark2: "rgba(45,62,80,.76)",
+  dark3: "#071c22",
+  dark4: "rgb(12,48,52)",
+  dark5: "rgb(11,43,47)",
+  light: "#ffffff",
+  red: "#f55753",
+
+  /**
+   * Use these for heading sizes
+   */
+  h1: "4.0rem",
+  h2: "3.0rem",
+  h3: "2.5rem",
+  h4: "2.0rem",
+  h5: "1.5rem",
+  h6: "1.0rem",
+
+  /**
+   * Use this for line line-height
+   */
+  lineHeight: "1.75rem",
+
+  /**
+   * Media Query Widths
+   */
+  phoneOnly: "500px",
+  tabletPortrait: "768px",
+  tabletLandscape: "992px",
+  desktopSmall: "1280px"
+};
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
